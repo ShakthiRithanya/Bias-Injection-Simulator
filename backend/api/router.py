e3e31fd57c4f329db8873095d57d3e7b2036d411
Shakthi Rithanya S <shakthirithanyasr07@gmail.com>
2026-02-01 20:47:02 +0530

    Backend: Register Model routes

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from backend.api.endpoints import data, bias
+from backend.api.endpoints import data, bias, model
 
 api_router = APIRouter()
 
 api_router.include_router(data.router, prefix="/data", tags=["data"])
 api_router.include_router(bias.router, prefix="/bias", tags=["bias"])
+api_router.include_router(model.router, prefix="/model", tags=["model"])
