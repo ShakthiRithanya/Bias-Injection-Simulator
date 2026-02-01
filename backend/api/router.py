37e9888cae089864311bd20a4712dc528fdfdfa2
Shakthi Rithanya S <shakthirithanyasr07@gmail.com>
2026-02-01 20:44:50 +0530

    Backend: Register Bias routes

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from backend.api.endpoints import data
+from backend.api.endpoints import data, bias
 
 api_router = APIRouter()
 
 api_router.include_router(data.router, prefix="/data", tags=["data"])
-# api_router.include_router(bias.router, prefix="/bias", tags=["bias"])
+api_router.include_router(bias.router, prefix="/bias", tags=["bias"])
